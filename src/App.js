8aaf2163ecbd5ef59e157acc87bd6c067a30ebbe
csimena <carly.simenauer@gmail.com>
2024-07-09 14:24:59 -0400

    big commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import CreateListing from './CreateListing';
+import SignUp from './SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div className="header-copy">
+              <h1 className="header-title">toolbelt</h1>
+              <h4>Build things in other people's garages. Borrow from their toolbelt? There's some sort of saying like
+                this
+              </h4>
+          </div>
+          <SignUp />
+          {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </a> */}
+        </header>
+
+      </div>
+      <CreateListing />
+    </>
   );
 }
 
---
+++ src/listingData.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const listingData = [
+    {
+
+    }
+];
+
+export default listingData;
